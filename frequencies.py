6d5374b97c1271c71441e55fcb995430b7140bd1
Alia Raed <alia.bahakeem@kcl.ac.uk>
2023-10-09 18:44:36 +0100

    add solution to frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    frequencies = {} 
+
+    frequency_dictonary = {}
+    for item in items:
+       item_str = str(item)
+
+        if item_str in frequency_dictionary:
+           frequency_dictonary[item_str] += 1
+        else:
+            frequency_dictonary[item_str] = 1
+
+    return frequency_dictonary
+
+# Test cases
+result = frequencies(['a', 'a', 'b', 'b', 'b', 'c'])
+print(result)  
     return frequencies
